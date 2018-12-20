ffc31ea623a998531d7ea269b865dc7f471c6708
v_arhipov <arxip@inbox.ru>
2018-12-20 09:48:57 +0300

    fix warning and add .idea to gitignore

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ exports.connect = function(url, done) {
   if(state.db) {
     return done();
   }
-  MongoClient.connect(url, function(err, client) {
+  MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
     if(err) {
       return done(err);
     }
